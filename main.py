b1954e8919dafe02d86853046a65a61e2ed79dcc
MacksM3 <michael.mccosh@gmail.com>
2022-05-10 12:00:08 -0400

    create module file

--- main.py
+++ main.py
@@ -2,64 +2,7 @@
 # Experimenting with Object Oriented Programming (OOP)
 # by Michael Macks McCosh (MacksM3)
 
-# parent class for all the pets to inherit from
-class Pet:
-    def __init__(self, name):
-        self.name = name
-        print("***New Pet Materializing!!!***")
-        print()
-
-
-# class definition of 'Dog' starts here
-class Dog(Pet):
-    # class attribute
-    at1 = "mammal"
-
-    def __init__(self, name):
-        super().__init__(name)
-
-    # Instance method
-    def speak(self):
-        return f"I am a Dog. \nMy name is {self.name}, \nI am a {self.at1},"
-
-
-# class definition of 'Cat' starts here
-class Cat(Pet):
-    # class attribute
-    at1 = "mammal"
-
-    def __init__(self, name):
-        super().__init__(name)
-
-    # Instance method
-    def speak(self):
-        return f"Hello! \nI am a Cat. \nMy name is {self.name}. \nAnd I am a {self.at1}."
-
-
-# class definition of 'Bird' starts here
-class Bird(Pet):
-    # class attribute
-    at1 = "bird"
-
-    def __init__(self, name):
-        super().__init__(name)
-
-    # Instance method
-    def speak(self):
-        return f"Hello! \nI am a Parrot. \nMy name is {self.name}. \nI am a {self.at1}."
-
-
-# class definition of 'Gecko' starts here
-class Gecko(Pet):
-    # class attribute
-    at1 = "reptil"
-
-    def __init__(self, name):
-        super().__init__(name)
-
-    # Instance method
-    def speak(self):
-        return f"Hello! \nI am a Gecko. \nMy name is {self.name}. \nAnd I am a {self.at1}."
+from pets import *
 
 
 # main function starts here
